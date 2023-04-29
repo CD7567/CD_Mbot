81b6169e3955b2c28923e976ac5182bf204a0d5b
CD7567 <alexander.y.koshelev@gmail.com>
2023-04-29 22:11:32 +0300

    Added requirements for hosting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """Main executable file of CD_MBot"""
 
-import json
 import os
+import json
 
 from aiogram import executor
+
 from src.bot import MBot
+from background import keep_alive
 
 token = open(os.path.join(os.path.dirname(__file__), 'conf', 'token.conf')).readline()
 conf = json.load(open(os.path.join(os.path.dirname(__file__), 'conf', 'conf.json'), 'r', encoding='utf-8'))
@@ -18,4 +20,6 @@ if conf['YDL']['cachedir'] == '':
     os.makedirs(cache_dir, exist_ok=True)
 
 bot = MBot(token, conf)
+
+keep_alive()
 executor.start_polling(bot.dispatcher)
