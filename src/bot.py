45e2b779d5172c333b28deb4911540140cd7ccb1
CD7567 <alexander.y.koshelev@gmail.com>
2023-05-16 21:39:27 +0300

    Added language options

--- src/bot.py
+++ src/bot.py
@@ -29,6 +29,7 @@ class FilenameCatcher(PostProcessor):
 class Form(StatesGroup):
     """Bot FSM Class"""
 
+    lang = State()
     loop = State()
     search = State()
 
@@ -39,6 +40,7 @@ class MBot(Bot):
     dispatcher: Dispatcher = None
     __storage: MemoryStorage = None
     __conf: dict = None
+    __locale: StringsLang = StringsEn
 
     def __init__(self, token: str, conf: dict):
         super().__init__(token)
@@ -53,44 +55,59 @@ class MBot(Bot):
 
         self.dispatcher = Dispatcher(self, storage=self.__storage)
         self.dispatcher.register_message_handler(self.__start, state='*', commands=['start'])
+        self.dispatcher.register_message_handler(self.__handle_lang_keyboard, state=Form.lang)
         self.dispatcher.register_message_handler(self.__help, state=Form.loop, commands=['help'])
         self.dispatcher.register_message_handler(self.__search, state=Form.loop, commands=['search'])
-        self.dispatcher.register_message_handler(self.__handle_button, state=Form.loop)
+        self.dispatcher.register_message_handler(self.__lang, state=Form.loop, commands=['lang'])
+        self.dispatcher.register_message_handler(self.__handle_loop_keyboard, state=Form.loop)
         self.dispatcher.register_message_handler(self.__perform_search, state=Form.search)
 
     async def __start(self, message: types.Message) -> None:
         """Message handler for /start"""
-        logging.info(f'Recieved \'/start\' from user {message.from_user.id}')
-
-        await Form.loop.set()
-        logging.info(f'User {message.from_user.id} state set to \'loop\'')
+        logging.info(f'Received \'/start\' from user {message.from_user.id}')
 
         try:
+            await Form.lang.set()
+            logging.info(f'User {message.from_user.id} state set to \'lang\'')
+
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            markup.add(str_search)
-            await self.send_message(message.from_user.id, str_greeting, reply_markup=markup)
+            markup.add(StringsLang.str_en, StringsLang.str_ru)
+
+            await self.send_message(message.from_user.id, self.__locale.str_greeting, reply_markup=markup)
         except Exception:
-            await self.send_message(message.from_user.id, str_error_internal)
+            logging.error(traceback.format_exc())
+            await self.send_message(message.from_user.id, self.__locale.str_error_internal)
             print(traceback.format_exc())
 
-    async def __help(self, message: types.Message):
+    async def __help(self, message: types.Message) -> None:
         """Message handler for /help"""
-        logging.info(f'Recieved \'/help\' from user {message.from_user.id}')
+        logging.info(f'Received \'/help\' from user {message.from_user.id}')
 
         try:
-            await self.send_message(message.from_user.id, str_help)
+            await self.send_message(message.from_user.id, self.__locale.str_help_content)
         except Exception:
-            await self.send_message(message.from_user.id, str_error_internal)
+            logging.error(traceback.format_exc())
+            await self.send_message(message.from_user.id, self.__locale.str_error_internal)
             print(traceback.format_exc())
 
     async def __search(self, message: types.Message) -> None:
         """Message handler for /search"""
-        logging.info(f'Recieved \'/search\' from user {message.from_user.id}')
+        logging.info(f'Received \'/search\' from user {message.from_user.id}')
 
         await Form.search.set()
         logging.info(f'User {message.from_user.id} state set to \'search\'')
 
-        await self.send_message(message.from_user.id, str_search_request)
+        await self.send_message(message.from_user.id, self.__locale.str_search_request)
+
+    async def __lang(self, message: types.Message) -> None:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(StringsLang.str_en, StringsLang.str_ru)
+
+        logging.info(f'Received \'/lang\' from user {message.from_user.id}')
+
+        await Form.lang.set()
+        logging.info(f'User {message.from_user.id} state set to \'lang\'')
+        await self.send_message(message.from_user.id, self.__locale.str_lang_choose, reply_markup=markup)
 
     async def __perform_search(self, message: types.Message) -> None:
         """Message handler for user search request"""
@@ -99,7 +116,7 @@ class MBot(Bot):
             track_name = message.text
             ydl = YoutubeDL(self.__conf['YDL'])
 
-            await self.send_message(message.from_user.id, str_search_start)
+            await self.send_message(message.from_user.id, self.__locale.str_search_start)
 
             filename_collector = FilenameCatcher()
             ydl.add_post_processor(filename_collector)
@@ -107,26 +124,58 @@ class MBot(Bot):
 
             await message.reply_document(open(filename_collector.filenames[0], 'rb'),
                                          caption=f'\U0001F3B5 \n{video_info["title"]}\n\U0001F3B5')
-            await self.send_message(message.from_user.id, str_search_end)
+            await self.send_message(message.from_user.id, self.__locale.str_search_end)
 
             time.sleep(5)
             os.remove(filename_collector.filenames[0])
         except Exception:
-            await self.send_message(message.from_user.id, str_error_internal)
+            logging.error(traceback.format_exc())
+            await self.send_message(message.from_user.id, self.__locale.str_error_internal)
             print(traceback.format_exc())
 
         await Form.loop.set()
         logging.info(f'User {message.from_user.id} state set to \'loop\'')
 
-    async def __handle_button(self, message: types.Message) -> None:
+    async def __handle_lang_keyboard(self, message: types.message) -> None:
+        """Message handler for user lang change request"""
+
+        try:
+            locale = message.text
+
+            if locale in locales.keys():
+                self.__locale = locales[locale]
+            else:
+                await self.send_message(message.from_user.id, self.__locale.str_error_not_found)
+                return
+
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup.add(self.__locale.str_search, self.__locale.str_help, self.__locale.str_lang)
+
+            await self.send_message(message.from_user.id, self.__locale.str_greeting_lang, reply_markup=markup)
+
+        except Exception:
+            logging.error(traceback.format_exc())
+            await self.send_message(message.from_user.id, self.__locale.str_error_internal)
+            print(traceback.format_exc())
+
+        await Form.loop.set()
+        logging.info(f'User {message.from_user.id} state set to \'loop\'')
+
+    async def __handle_loop_keyboard(self, message: types.Message) -> None:
         """Message handler for buttons"""
 
         match str(message.text):
-            case '\U0001F50D Искать':
-                logging.info(f'Recieved \'/search\' from user {message.from_user.id}')
+            case self.__locale.str_search:
+                logging.info(f'Received \'/search\' from user {message.from_user.id}')
 
                 await Form.search.set()
-                await self.send_message(message.from_user.id, str_search_request)
+                await self.send_message(message.from_user.id, self.__locale.str_search_request)
+
+            case self.__locale.str_help:
+                await self.__help(message)
+
+            case self.__locale.str_lang:
+                await self.__lang(message)
 
             case _:
-                await self.send_message(message.from_user.id, str_error_not_found)
+                await self.send_message(message.from_user.id, self.__locale.str_error_not_found_help)
--- src/strings.py
+++ src/strings.py
@@ -1,13 +1,70 @@
 """This file contains string sources of CD_MBot"""
 
-str_greeting = 'Привет! \U0000270B \nЯ умею искать музыку на Youtube \U0001F60E \nНачнем?'
 
-str_search_request = '\U0001F50D Напиши название трека, который хочешь найти'
-str_search_start = '\U0001F50D Уже ищу нужный тебе трек \nПодожди немного... \U0001f4a4'
-str_search_end = '\U00002705 Нашел нужный тебе трек \nСпасибо за твой интерес \U0001F49E'
-str_search = '\U0001F50D Искать'
+class StringsLang:
+    str_greeting = ''
+    str_greeting_lang = ''
 
-str_error_internal = 'Что-то пошло не так... \U0001F614 \nВозвращаемся в основное меню'
-str_error_not_found = 'Я не понял твою команду \U0001F614 \nКоманды можно посмотреть по /help'
+    str_search_request = ''
+    str_search_start = ''
+    str_search_end = ''
+    str_search = ''
 
-str_help = '/help -- помощь по командам \n/search -- искать трек'
+    str_error_internal = ''
+    str_error_internal_start = ''
+    str_error_not_found = ''
+    str_error_not_found_help = ''
+
+    str_help = ''
+    str_help_content = ''
+
+    str_lang = ''
+    str_lang_choose = ''
+
+    str_en = '\U0001F1EC\U0001F1E7'
+    str_ru = '\U0001F1F7\U0001F1FA'
+
+
+class StringsRu(StringsLang):
+    str_greeting = 'Привет! \U0000270B \nЯ умею искать музыку на Youtube \U0001F60E \nДавай выберем язык'
+    str_greeting_lang = 'Замечательно \U0001F609 \nДавай начнем!'
+
+    str_search_request = '\U0001F50D Напиши название трека, который хочешь найти'
+    str_search_start = '\U0001F50D Уже ищу нужный тебе трек \nПодожди немного... \U0001f4a4'
+    str_search_end = '\U00002705 Нашел нужный тебе трек \nСпасибо за твой интерес \U0001F49E'
+    str_search = '\U0001F50D Искать'
+
+    str_error_internal = 'Что-то пошло не так... \U0001F614 \nВозвращаемся в основное меню'
+    str_error_internal_start = 'Что-то пошло не так... \U0001F614'
+    str_error_not_found = 'Я не понял твою команду \U0001F614'
+    str_error_not_found_help = 'Я не понял твою команду \U0001F614 \nКоманды можно посмотреть по /help'
+
+    str_help = '\U00002753 Помощь'
+    str_help_content = '/help -- помощь по командам \n/search -- искать трек \n/lang -- изменить язык'
+
+    str_lang = '\U0001F1F7\U0001F1FA Язык'
+    str_lang_choose = 'Давай выберем язык'
+
+
+class StringsEn(StringsLang):
+    str_greeting = 'Hello! \U0000270B \nI can look for music on Youtube \U0001F60E \nLet\'s choose a language'
+    str_greeting_lang = 'Great \U0001F609 \nLet\'s start!'
+
+    str_search_request = '\U0001F50D Type name of the track you\'re looking for'
+    str_search_start = '\U0001F50D Looking for a track \nWait for a while... \U0001f4a4'
+    str_search_end = '\U00002705 I found the track for you \nThanks for your interest \U0001F49E'
+    str_search = '\U0001F50D Search'
+
+    str_error_internal = 'Something went wrong... \U0001F614 \nReturning to main menu...'
+    str_error_internal_start = 'Something went wrong... \U0001F614'
+    str_error_not_found = 'I didn\'t understand you \U0001F614'
+    str_error_not_found_help = 'I didn\'t understand you \U0001F614 \nYou can check out command by /help'
+
+    str_help = '\U00002753 Help'
+    str_help_content = '/help -- command help \n/search -- look for a track \n/lang -- change language'
+
+    str_lang = '\U0001F1EC\U0001F1E7 Language'
+    str_lang_choose = 'Let\'s choose a language'
+
+
+locales = {StringsLang.str_en: StringsEn, StringsLang.str_ru: StringsRu}
